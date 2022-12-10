8e23a3f3f28b83584ac48b0ae7ea6fdc3707d4a7
yanJuicy <hanjub3@gmail.com>
2022-12-10 11:47:16 +0900

    initial commit

---
+++ src/main/java/com/hanghae/blog/api/HanghaeBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.blog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeBlogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeBlogApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae/blog/api/HanghaeBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.blog.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeBlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
