d568eb85dbd1214a0557c9bd864dbfe565bda97f
mina <m1nna@naver.com>
2022-12-12 15:46:32 +0900

    feat: 댓글 등록 API
    게시글 별 댓글 등록 API 작성

--- src/main/java/com/hanghae/blog/api/comment/controller/CommentController.java
+++ src/main/java/com/hanghae/blog/api/comment/controller/CommentController.java
@@ -5,7 +5,6 @@ import com.hanghae.blog.api.comment.dto.ResponseCreateCommentDto;
 import com.hanghae.blog.api.comment.service.CommentService;
 import com.hanghae.blog.api.common.response.GenericResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
-    @Transactional
+
     @PostMapping("/{postId}")
     public GenericResponseDto<ResponseCreateCommentDto> createComment(@PathVariable Long postId, @RequestBody RequestCreateCommentDto requestCreateCommentDto){
         return commentService.createComment(postId, requestCreateCommentDto);
