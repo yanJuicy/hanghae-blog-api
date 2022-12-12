f1a304c46321c400eac91febb4ebaddeee079723
mina <m1nna@naver.com>
2022-12-12 10:44:38 +0900

    refactor: 스프링부트 버전 변경
    Security버전에 맞춰 Springboot v.3.0.0 → v.2.7.6 으로 낮춤

--- src/main/java/com/hanghae/blog/api/posting/entity/Posting.java
+++ src/main/java/com/hanghae/blog/api/posting/entity/Posting.java
@@ -1,15 +1,16 @@
 package com.hanghae.blog.api.posting.entity;
 
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 @Builder
 @Getter
