4efe10b796f8cc889a37c9228ce4a33667850018
황보석 <zheoajfl@gmail.com>
2022-12-13 17:35:17 +0900

    전체 게시글 조회 API

--- src/main/java/com/hanghae/blog/api/posting/mapper/PostingMapper.java
+++ src/main/java/com/hanghae/blog/api/posting/mapper/PostingMapper.java
@@ -2,8 +2,8 @@ package com.hanghae.blog.api.posting.mapper;
 
 
 import com.hanghae.blog.api.common.response.DataResponse;
-import com.hanghae.blog.api.posting.dto.RequestCreatePostingDto;
-import com.hanghae.blog.api.posting.dto.ResponseCreatePostingDto;
+import com.hanghae.blog.api.posting.dto.RequestCreatePosting;
+import com.hanghae.blog.api.posting.dto.ResponseCreatePosting;
 
 import com.hanghae.blog.api.posting.entity.Posting;
 import org.springframework.stereotype.Component;
@@ -14,8 +14,8 @@ import static com.hanghae.blog.api.common.response.ResponseMessage.CREATE_POSTIN
 public class PostingMapper {
 
 
-    public DataResponse<ResponseCreatePostingDto> toResponse(Posting posting) {
-        ResponseCreatePostingDto response = ResponseCreatePostingDto.builder()
+    public DataResponse<ResponseCreatePosting> toResponse(Posting posting) {
+        ResponseCreatePosting response = ResponseCreatePosting.builder()
 
                 .id(posting.getId())
                 .title(posting.getTitle())
--- src/main/java/com/hanghae/blog/api/posting/service/PostingService.java
+++ src/main/java/com/hanghae/blog/api/posting/service/PostingService.java
@@ -1,9 +1,11 @@
 package com.hanghae.blog.api.posting.service;
 
 
+import com.fasterxml.jackson.databind.DatabindException;
 import com.hanghae.blog.api.common.response.DataResponse;
-import com.hanghae.blog.api.posting.dto.RequestCreatePostingDto;
-import com.hanghae.blog.api.posting.dto.ResponseCreatePostingDto;
+import com.hanghae.blog.api.posting.dto.RequestCreatePosting;
+import com.hanghae.blog.api.posting.dto.ResponseCreatePosting;
+
 
 import com.hanghae.blog.api.posting.entity.Posting;
 import com.hanghae.blog.api.posting.mapper.PostingMapper;
@@ -12,15 +14,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@RequiredArgsConstructor
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class PostingService {
     private final PostingRepository postingRepository;
 	private final PostingMapper postingMapper;
 
+
     @Transactional
 
-    public DataResponse<ResponseCreatePostingDto> create(RequestCreatePostingDto requestDto) {
+    public DataResponse<ResponseCreatePosting> create(RequestCreatePosting requestDto) {
 
 		Posting posting = postingMapper.toPosting(requestDto);
 
@@ -28,6 +34,20 @@ public class PostingService {
 
         return postingMapper.toResponse(savedPosting);
     }
+
+    @Transactional
+    public List<ResponseCreatePosting> findAllPosting(){
+        List<Posting> Posting = postingRepository.findAll();
+
+        List<ResponseCreatePosting> result = new ArrayList<>();
+
+        for(Posting p : Posting){
+            result.add(new ResponseCreatePosting(p));
+        }
+        return result;
+    }
+
+
 
 
 }
