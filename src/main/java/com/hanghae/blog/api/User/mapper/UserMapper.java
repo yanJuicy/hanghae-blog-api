3fb1626b555f39687e11213699cf0ee243c26150
jihunback <jihun1362@nate.com>
2022-12-13 18:46:13 +0900

    feat: 필요없는 메소드 삭제
    필요없는 toResponse메소드 삭제

--- src/main/java/com/hanghae/blog/api/User/mapper/UserMapper.java
+++ src/main/java/com/hanghae/blog/api/User/mapper/UserMapper.java
@@ -3,23 +3,16 @@ package com.hanghae.blog.api.User.mapper;
 import com.hanghae.blog.api.User.dto.RequestCreateUser;
 import com.hanghae.blog.api.User.entity.User;
 import com.hanghae.blog.api.User.entity.UserRoleEnum;
-import com.hanghae.blog.api.common.response.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import static com.hanghae.blog.api.common.response.ResponseMessage.SIGNUP_USER_SUCCESS_MSG;
-
 @Component
 @RequiredArgsConstructor
 public class UserMapper {
-
+    //저희는 엔터티 -> dto 변환을 mapper 클래스에서 하기로 했습니다!
     @Value("${admin.token}")
     private String ADMIN_TOKEN;
-
-    public Response toResponse() {
-        return new Response(SIGNUP_USER_SUCCESS_MSG);
-    }
 
     public User toUser(RequestCreateUser requestCreateUser) {
         UserRoleEnum role = UserRoleEnum.USER;
