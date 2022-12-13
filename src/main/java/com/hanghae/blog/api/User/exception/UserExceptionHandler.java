9b071a1842884d4262a8962fbaa86ff6264bb750
jihunback <jihun1362@nate.com>
2022-12-13 20:37:39 +0900

    feat: 로그인 예외 처리 API
    로그인 예외 처리 API 생성

--- src/main/java/com/hanghae/blog/api/User/exception/UserExceptionHandler.java
+++ src/main/java/com/hanghae/blog/api/User/exception/UserExceptionHandler.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import static com.hanghae.blog.api.common.exception.ExceptionMessage.ADMIN_TOKEN_MISMATCH_ERROR_MSG;
 import static com.hanghae.blog.api.common.exception.ExceptionMessage.DUPLICATE_USER_ERROR_MSG;
 import static com.hanghae.blog.api.common.exception.ExceptionMessage.INTERNAL_SERVER_ERROR_MSG;
+import static com.hanghae.blog.api.common.exception.ExceptionMessage.PASSWORDS_DO_NOT_MATCH_ERROR_MSG;
+import static com.hanghae.blog.api.common.exception.ExceptionMessage.USER_NOT_FOUND_ERROR_MSG;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice(assignableTypes = UserController.class)
@@ -24,12 +26,20 @@ public class UserExceptionHandler {
 
         if (e.getMessage().equals(ADMIN_TOKEN_MISMATCH_ERROR_MSG.getMsg())) {
             return new ExceptionResponse(ADMIN_TOKEN_MISMATCH_ERROR_MSG);
-        } else if (e.getMessage().equals(DUPLICATE_USER_ERROR_MSG.getMsg())) {
+        }
+        if (e.getMessage().equals(DUPLICATE_USER_ERROR_MSG.getMsg())) {
             return new ExceptionResponse(DUPLICATE_USER_ERROR_MSG);
+        }
+        if (e.getMessage().equals(USER_NOT_FOUND_ERROR_MSG.getMsg())) {
+            return new ExceptionResponse(USER_NOT_FOUND_ERROR_MSG);
+        }
+        if (e.getMessage().equals(PASSWORDS_DO_NOT_MATCH_ERROR_MSG.getMsg())) {
+            return new ExceptionResponse(PASSWORDS_DO_NOT_MATCH_ERROR_MSG);
         }
         return new ExceptionResponse(INTERNAL_SERVER_ERROR_MSG);
     }
 
+    //@Valid 예외처리
     @ExceptionHandler({MethodArgumentNotValidException.class})
     public ExceptionResponse handleBadRequest(MethodArgumentNotValidException e) {
         BindingResult bindingResult = e.getBindingResult();
@@ -37,9 +47,8 @@ public class UserExceptionHandler {
         for (FieldError fieldError : bindingResult.getFieldErrors()) {
             if (fieldError.getField().equals("username")) {
                 return new ExceptionResponse(fieldError.getField() + "(은)는 최소 4 자 및 최대 10 자 하나의 소문자, 하나의 숫자가 들어가야합니다.", 400);
-            } else {
-                return new ExceptionResponse(fieldError.getField() + "최소 8 자 및 최대 15 자, 하나 이상의 대문자, 하나의 소문자, 하나의 숫자 및 하나의 특수 문자가 들어가야합니다.", 400);
             }
+            return new ExceptionResponse(fieldError.getField() + "최소 8 자 및 최대 15 자, 하나 이상의 대문자, 하나의 소문자, 하나의 숫자 및 하나의 특수 문자가 들어가야합니다.", 400);
         }
         return new ExceptionResponse(INTERNAL_SERVER_ERROR_MSG);
 
