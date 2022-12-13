ed78a75f1a52e74735856d7fd3439623a6591603
jihunback <jihun1362@nate.com>
2022-12-13 20:00:32 +0900

    feat: 로그인 성공 리스폰메세지 추가
    로그인 성공 리스폰메세지 추가

--- src/main/java/com/hanghae/blog/api/User/service/UserService.java
+++ src/main/java/com/hanghae/blog/api/User/service/UserService.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 
+import static com.hanghae.blog.api.common.response.ResponseMessage.LOGIN_USER_SUCCESS_MSG;
 import static com.hanghae.blog.api.common.response.ResponseMessage.SIGNUP_USER_SUCCESS_MSG;
 
 @Service
