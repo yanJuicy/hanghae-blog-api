a2ae3000a5a7a86436527fc5bbe8532828d1d229
jihunback <jihun1362@nate.com>
2022-12-13 16:57:41 +0900

    feat: 회원가입 API
    회원가입 API작성

---
+++ src/main/java/com/hanghae/blog/api/User/contorller/UserController.java
@@ -0,0 +1,26 @@
+package com.hanghae.blog.api.User.contorller;
+
+import com.hanghae.blog.api.User.dto.RequestCreateUser;
+import com.hanghae.blog.api.User.service.UserService;
+import com.hanghae.blog.api.common.response.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public Response signup(@RequestBody @Valid RequestCreateUser requestCreateUser) {
+        return userService.signup(requestCreateUser);
+    }
+
+
+}
---
+++ src/main/java/com/hanghae/blog/api/User/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.hanghae.blog.api.User.repository;
+
+import com.hanghae.blog.api.User.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
