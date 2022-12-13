b6e114fdae520fb1b7192362ab93af575b0a4342
jihunback <jihun1362@nate.com>
2022-12-13 16:37:19 +0900

    feat: 회원가입 API
    회원가입 API작성

--- src/main/java/com/hanghae/blog/api/common/exception/ExceptionMessage.java
+++ src/main/java/com/hanghae/blog/api/common/exception/ExceptionMessage.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 public enum ExceptionMessage {
 
     // common
-    INTERNAL_SERVER_ERROR_MSG(500,"서버 에러입니다.");
+    INTERNAL_SERVER_ERROR_MSG(500,"서버 에러입니다."),
+    ADMIN_TOKEN_MISMATCH_ERROR_MSG(400,"관리자 암호가 틀려 등록이 불가능합니다."),
+    DUPLICATE_USER_ERROR_MSG(400,"중복된 사용자가 존재합니다.");
 
     private final int status;
     private final String msg;
--- src/main/java/com/hanghae/blog/api/posting/service/PostingService.java
+++ src/main/java/com/hanghae/blog/api/posting/service/PostingService.java
@@ -2,9 +2,8 @@ package com.hanghae.blog.api.posting.service;
 
 
 import com.hanghae.blog.api.common.response.DataResponse;
-import com.hanghae.blog.api.posting.dto.RequestCreatePostingDto;
-import com.hanghae.blog.api.posting.dto.ResponseCreatePostingDto;
-
+import com.hanghae.blog.api.posting.dto.RequestCreatePosting;
+import com.hanghae.blog.api.posting.dto.ResponseCreatePosting;
 import com.hanghae.blog.api.posting.entity.Posting;
 import com.hanghae.blog.api.posting.mapper.PostingMapper;
 import com.hanghae.blog.api.posting.repository.PostingRepository;
@@ -20,7 +19,7 @@ public class PostingService {
 
     @Transactional
 
-    public DataResponse<ResponseCreatePostingDto> create(RequestCreatePostingDto requestDto) {
+    public DataResponse<ResponseCreatePosting> create(RequestCreatePosting requestDto) {
 
 		Posting posting = postingMapper.toPosting(requestDto);
 
