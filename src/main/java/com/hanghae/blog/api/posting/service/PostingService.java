c617853dc415ef4ec2273f1603e371ec18a43d3e
yanJuicy <hanjub3@gmail.com>
2022-12-14 13:56:05 +0900

    refactor: Service에서 data만 반환, Controller에서 Data Response 반환 하도록 변경

--- src/main/java/com/hanghae/blog/api/posting/service/PostingService.java
+++ src/main/java/com/hanghae/blog/api/posting/service/PostingService.java
@@ -1,11 +1,8 @@
 package com.hanghae.blog.api.posting.service;
 
-import com.hanghae.blog.api.common.response.DataResponse;
-import com.hanghae.blog.api.common.response.PageResponse;
 import com.hanghae.blog.api.posting.dto.RequestCreatePosting;
 import com.hanghae.blog.api.posting.dto.RequestPagePosting;
 import com.hanghae.blog.api.posting.dto.ResponseCreatePosting;
-import com.hanghae.blog.api.posting.dto.ResponsePosting;
 import com.hanghae.blog.api.posting.entity.Posting;
 import com.hanghae.blog.api.posting.mapper.PostingMapper;
 import com.hanghae.blog.api.posting.repository.PostingRepository;
@@ -18,20 +15,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
-
-import static com.hanghae.blog.api.common.response.ResponseMessage.READ_PAGING_POSTING_SUCCESS_MSG;
 
 @Service
 @RequiredArgsConstructor
 public class PostingService {
+    private static final String SORT_BY = "createdAt";
+
     private final PostingRepository postingRepository;
 	private final PostingMapper postingMapper;
 
 
     @Transactional
-
-    public DataResponse<ResponseCreatePosting> create(RequestCreatePosting requestDto) {
+    public ResponseCreatePosting create(RequestCreatePosting requestDto) {
 
 		Posting posting = postingMapper.toPosting(requestDto);
 
@@ -52,12 +47,9 @@ public class PostingService {
         return result;
     }
 
-    public PageResponse<ResponsePosting, Posting> findPagePosting(RequestPagePosting requestDto) {
-        Pageable pageable = requestDto.getPageable(Sort.by("createdAt"));
-        Page<Posting> result = postingRepository.findAll(pageable);
-        Function<Posting, ResponsePosting> fn = p -> postingMapper.toResponsePosting(p);
-
-        return new PageResponse<>(READ_PAGING_POSTING_SUCCESS_MSG, result, fn);
+    public Page<Posting> findPagePosting(RequestPagePosting requestDto) {
+        Pageable pageable = requestDto.getPageable(Sort.by(SORT_BY));
+        return postingRepository.findAll(pageable);
     }
 
 }
