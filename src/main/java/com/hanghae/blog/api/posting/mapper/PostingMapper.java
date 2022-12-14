3be47aea27435450c390916243871b709f2af40f
yanJuicy <hanjub3@gmail.com>
2022-12-14 13:56:57 +0900

    refactor: PostingMapper에서는 ResponsePosting 관련으로만 매핑하도록 변경

--- src/main/java/com/hanghae/blog/api/posting/mapper/PostingMapper.java
+++ src/main/java/com/hanghae/blog/api/posting/mapper/PostingMapper.java
@@ -1,27 +1,23 @@
 package com.hanghae.blog.api.posting.mapper;
 
-import com.hanghae.blog.api.common.response.DataResponse;
 import com.hanghae.blog.api.posting.dto.RequestCreatePosting;
 import com.hanghae.blog.api.posting.dto.ResponseCreatePosting;
 import com.hanghae.blog.api.posting.dto.ResponsePosting;
 import com.hanghae.blog.api.posting.entity.Posting;
 import org.springframework.stereotype.Component;
 
-import static com.hanghae.blog.api.common.response.ResponseMessage.CREATE_POSTING_SUCCESS_MSG;
-
 @Component
 public class PostingMapper {
 
-    public DataResponse<ResponseCreatePosting> toResponse(Posting posting) {
+    public ResponseCreatePosting toResponse(Posting posting) {
         ResponseCreatePosting response = ResponseCreatePosting.builder()
-
                 .id(posting.getId())
                 .title(posting.getTitle())
                 .writer(posting.getWriter())
                 .contents(posting.getContents())
                 .build();
 
-        return new DataResponse<>(CREATE_POSTING_SUCCESS_MSG, response);
+        return response;
     }
 
     public ResponsePosting toResponsePosting(Posting posting) {
