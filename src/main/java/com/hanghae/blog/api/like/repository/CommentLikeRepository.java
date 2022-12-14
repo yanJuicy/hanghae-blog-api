1bfccdc99292cc680dd1270250fa30ac677a2664
mina <m1nna@naver.com>
2022-12-15 00:55:59 +0900

    feat: 댓글 좋아요 조회 메소드

--- src/main/java/com/hanghae/blog/api/like/repository/CommentLikeRepository.java
+++ src/main/java/com/hanghae/blog/api/like/repository/CommentLikeRepository.java
@@ -3,8 +3,11 @@ package com.hanghae.blog.api.like.repository;
 import com.hanghae.blog.api.like.entity.CommentLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
 
+     Optional<CommentLike> findByCommentIdAndUsername(Long CommentId, String username);
      void deleteByCommentIdAndUsername(Long CommentId, String username);
 
 }
