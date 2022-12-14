68a2f021f8a681eca5f74ad2487277cb8ae34b32
mina <m1nna@naver.com>
2022-12-14 23:37:27 +0900

    feat: 댓글 좋아요 dto, entity, mapper 생성
    - request, response일 때의 데이터 유형이 같아 dto 통일

---
+++ src/main/java/com/hanghae/blog/api/like/entity/CommentLike.java
@@ -0,0 +1,31 @@
+package com.hanghae.blog.api.like.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private Long commentId;
+
+}
